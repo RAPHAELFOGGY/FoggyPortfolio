6ef321e2fd087ce456cf2089d8377276a870b83e
Tobibad1 <tobi_adekanmbi@yahoo.com>
2025-07-16 07:50:43 +0100

    FoggyPortfolio

--- javascripts.js
+++ javascripts.js
@@ -1,18 +1,7 @@
-    // sticky header and menu toggle functionality
-    const header = document.querySelector('header');
-    window.addEventListener('scroll', function() { ;
-    header.classList.toggle('sticky', window.scrollY > 100);
-    });
-// Mobile menu toggle functionality
-    let menuIcon = document.querySelector('#menu-icon');
-    let navlist = document.querySelector('.navlist');
+// Mobile menu toggle
+const menuIcon = document.querySelector("#menu-icon");
+const navlist = document.querySelector(".navlist");
 
-    menuIcon.onclick = () => {
-    navlist.classList.toggle('open');
-    const icon = menuIcon.querySelector('i');
-    menuIcon.classList.toggle('fa-xmark');
-    menuIcon.classList.toggle('fa-bars');
-    };
-
-
-
+menuIcon.addEventListener("click", () => {
+    navlist.classList.toggle("active");
+});
